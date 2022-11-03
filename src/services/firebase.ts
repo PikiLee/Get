15ff8e474836b258d1935d784772b9858a4ae73b
luckypeppa <871565198@qq.com>
2022-11-03 16:29:33 +0800

    set up auth emulator

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+connectAuthEmulator(auth, 'http://localhost:9099');
