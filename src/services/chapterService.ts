8e04cf9f8b3d694d15f4c1118fef2b38cf713857
luckypeppa <871565198@qq.com>
2022-11-07 19:15:00 +0800

    Modify methods so chapters are now stored in courses subcollections

--- src/services/chapterService.ts
+++ src/services/chapterService.ts
@@ -1,3 +1,4 @@
+import { useUserStore } from './../stores/userStore';
 import { useChapterStore } from './../stores/chapterStore';
 import { NewChapter, Chapter } from './../types/chapter';
 import {
@@ -14,13 +15,37 @@ import { db } from './firebase';
 
 const chapterStore = useChapterStore();
 
+const getChapterCollection = (courseId: string) => {
+  const userStore = useUserStore();
+  if (!userStore.user) throw Error;
+  return collection(
+    db,
+    'users',
+    userStore.user.id,
+    'courses',
+    courseId,
+    'chapters'
+  );
+};
+
+const getChapterDoc = (courseId: string, chapterId: string) => {
+  const userStore = useUserStore();
+  if (!userStore.user) throw Error;
+  return doc(
+    db,
+    'users',
+    userStore.user.id,
+    'courses',
+    courseId,
+    'chapters',
+    chapterId
+  );
+};
+
 const postChapter = async (courseId: string, newChapter: NewChapter) => {
   // Add a new document with a generated id.
   try {
-    const chapterRef = await addDoc(
-      collection(db, 'courses', courseId, 'chapters'),
-      newChapter
-    );
+    const chapterRef = await addDoc(getChapterCollection(courseId), newChapter);
     const docSnap = await getDoc(chapterRef);
 
     chapterStore.addChapter({ ...docSnap.data(), id: docSnap.id } as Chapter);
@@ -30,30 +55,37 @@ const postChapter = async (courseId: string, newChapter: NewChapter) => {
 };
 
 const fetchChapters = async (courseId: string) => {
-  const q = query(collection(db, 'courses', courseId, 'chapters'));
+  try {
+    const q = query(getChapterCollection(courseId));
 
-  const querySnapshot = await getDocs(q);
-  const chapters: Chapter[] = [];
-  querySnapshot.forEach((doc) => {
-    chapters.push({ ...doc.data(), id: doc.id } as Chapter);
-  });
-  chapterStore.chapters = chapters;
+    const querySnapshot = await getDocs(q);
+    const chapters: Chapter[] = [];
+    querySnapshot.forEach((doc) => {
+      chapters.push({ ...doc.data(), id: doc.id } as Chapter);
+    });
+    chapterStore.chapters = chapters;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const updateChapter = async (courseId: string, chapter: Chapter) => {
-  const docRef = doc(db, 'courses', courseId, 'chapters', chapter.id);
-
-  // Set the "capital" field of the city 'DC'
-  return await updateDoc(docRef, {
-    name: chapter.name,
-    stage: chapter.stage,
-    lastDate: chapter.lastDate,
-  });
+  try {
+    const docRef = getChapterDoc(courseId, chapter.id);
+    // Set the "capital" field of the city 'DC'
+    return await updateDoc(docRef, {
+      name: chapter.name,
+      stage: chapter.stage,
+      lastDate: chapter.lastDate,
+    });
+  } catch (err) {
+    throw err;
+  }
 };
 
 const deleteChapter = async (courseId: string, chapterId: string) => {
   try {
-    await deleteDoc(doc(db, 'courses', courseId, 'chapters', chapterId));
+    await deleteDoc(getChapterDoc(courseId, chapterId));
     chapterStore.deleteChapter(chapterId);
   } catch (err) {
     throw err;
